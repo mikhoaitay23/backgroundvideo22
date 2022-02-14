513baf2cf625ccea19d19bc423b1ed8cc3e9f21f
huytq27091193@outlook.com <huytq27091193@outlook.com>
2022-02-14 23:57:43 +0700

    update

--- app/src/main/java/com/hola360/backgroundvideorecoder/ui/dialog/BatteryWarningDialog.kt
+++ app/src/main/java/com/hola360/backgroundvideorecoder/ui/dialog/BatteryWarningDialog.kt
@@ -5,17 +5,31 @@ import com.hola360.backgroundvideorecoder.R
 import com.hola360.backgroundvideorecoder.databinding.LayoutBatteryWarningBinding
 import com.hola360.backgroundvideorecoder.ui.base.basedialog.BaseDialog
 
-class BatteryWarningDialog: BaseDialog<LayoutBatteryWarningBinding>() {
+class BatteryWarningDialog(val callback:OnBatteryLowAccept,
+                           private val dismissCallback:OnDialogDismiss): BaseDialog<LayoutBatteryWarningBinding>() {
 
     override val layoutId: Int
         get() = R.layout.layout_battery_warning
 
     override fun initView() {
-
+        binding!!.cancel.setOnClickListener { dismiss() }
+        binding!!.oke.setOnClickListener {
+            callback.onBatteryLowAccept()
+            dismiss()
+        }
     }
 
     override fun onStart() {
         super.onStart()
         dialog?.window?.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dismissCallback.onDismiss()
+    }
+
+    interface OnBatteryLowAccept{
+        fun onBatteryLowAccept()
     }
 }
